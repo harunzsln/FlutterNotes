0c8b95f1e581195614f4e9c497728f4381f42d80
unknown <harunzsln0@gmail.com>
2025-08-05 18:02:40 +0300

    png added

--- projeler/ornek_proje/lib/screens/home_screen.dart
+++ projeler/ornek_proje/lib/screens/home_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ornek_proje/screens/second_screen.dart';
-
-import '../custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,30 +6,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Ornek Proje',
-          style: TextStyle(fontWeight: FontWeight.w300, color: Colors.white),
-        ),
-        backgroundColor: Colors.indigo.shade200,
-        centerTitle: true,
-      ),
+      backgroundColor: Color(0xFF191919),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CustomCardView(),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondScreen()),
-                );
-              },
-              child: const Text("ikinci ekrana git"),
-            ),
-          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset("assets/images/zikirmatik.png")],
         ),
       ),
     );
