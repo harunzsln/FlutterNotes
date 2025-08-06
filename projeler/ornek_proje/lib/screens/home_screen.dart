a8b50cf2fbf416376d85b24866afefcba9310f68
unknown <harunzsln0@gmail.com>
2025-08-06 18:08:59 +0300

    counter eklendi
    zikirmatik

--- projeler/ornek_proje/lib/screens/home_screen.dart
+++ projeler/ornek_proje/lib/screens/home_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    _counter++;
+    setState(() {});
+  }
+
+  void _resetCounter() {
+    _counter = 0;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +46,7 @@ class HomeScreen extends StatelessWidget {
 
   GestureDetector _resetButton() {
     return GestureDetector(
-      onTap: () => print("Reset"),
+      onTap: () => _resetCounter(),
       child: Container(
         width: 30,
         height: 30,
@@ -39,17 +56,20 @@ class HomeScreen extends StatelessWidget {
   }
 
   Text _counterText() {
-    return Text('0', style: TextStyle(fontSize: 50, color: Colors.white));
+    return Text(
+      '$_counter',
+      style: TextStyle(fontSize: 50, color: Colors.white),
+    );
   }
-}
 
-GestureDetector _incrementButton() {
-  return GestureDetector(
-    onTap: () => print('Tıklandı'),
-    child: Container(
-      width: 90,
-      height: 90,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
-    ),
-  );
+  GestureDetector _incrementButton() {
+    return GestureDetector(
+      onTap: () => _incrementCounter(),
+      child: Container(
+        width: 90,
+        height: 90,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
+      ),
+    );
+  }
 }
